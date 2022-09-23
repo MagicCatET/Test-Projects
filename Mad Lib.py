91f176957abdf020b0e28e7d6db5c6c932c3cad4
MagicCatET <114249823+MagicCatET@users.noreply.github.com>
2022-09-23 19:27:02 +0000

    Adding mad lib project

---
+++ Mad Lib.py
@@ -0,0 +1,26 @@
+""" Mad Lib Generator
+
+--
+
+"""
+
+# Questions
+
+Adj = input("Choose a adjective: ")
+Adj2 = input("Choose a adjective: ")
+Noun = input("Choose a noun: ")
+TOBird = input("Choose a type of bird: ")
+RIHouse = input("Choose a room in a house: ")
+Verb = input("Choose a verb: ")
+Verb2 = input("Choose a verb: ")
+RN = input("Choose a relative's name: ")
+Liquid = input("Choose a liquid: ")
+EVerbing = input("Choose a verb ending in ing: ")
+Body = input("Choose a part of the body: ")
+PNoun = input("Choose a plural noun: ")
+Noun2 = input("Choose a noun: ")
+EVerbing2 = input("Choose a verb ending in ing: ")
+
+# Mad Lib
+
+print("It was a", Adj, "cold November day. I woke up to the", Adj2, "smell of", TOBird, "roasting in the", RIHouse, "downstairs. I", Verb, "down the stairs to see if I could help", Verb2, "the dinner. My mom said, See if", RN, "needs a fresh", Noun, "So I carried a tray of glasses full of", Liquid, "into the", EVerbing, "room. When I got there, I couldn't believe my", Body, "! There were", PNoun, EVerbing2, Noun2,"!" )
